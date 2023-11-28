6c4a521cea5c84a73944e115ce5831fb3446d192
ZoeSobowale <38224967+ZoeSobowale@users.noreply.github.com>
2023-11-28 17:12:16 +0000

    Add js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var topic = "HTML";
+
+if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+} else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log("Please try again!");
+    }
